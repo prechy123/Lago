04ea5851e5ffdad5e7ea615a9a37db7dfb22250c
prechy123 <bamideleprecious100@gmail.com>
2023-08-27 21:39:03 +0100

    adding animation to about text

--- src/components/Sections/about/About-text.jsx
+++ src/components/Sections/about/About-text.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function Abouttext() {
   return (
-    <section id="abouttext">
-      <div className="l-side">
+    <motion section id="abouttext">
+      <motion.div className="l-side" initial={{y: "-100vh", opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{duration:0.5, delay: 1}}>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus
         iusto placeat corporis necessitatibus earum esse veritatis fugiat.
         Voluptatibus obcaecati quae esse tempore atque, veritatis vel labore
         commodi. Eum, voluptatibus nihil!
-      </div>
-      <div className="r-side">
+      </motion.div>
+      <motion.div className="r-side" initial={{y: "-100vh", opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{duration: 1}}>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus
         iusto placeat corporis necessitatibus earum esse veritatis fugiat.
         Voluptatibus obcaecati quae esse tempore atque, veritatis vel labore
@@ -46,7 +47,7 @@ export default function Abouttext() {
         necessitatibus earum esse veritatis fugiat. Voluptatibus obcaecati quae
         esse tempore atque, veritatis vel labore commodi. Eum, voluptatibus
         nihil!
-      </div>
-    </section>
+      </motion.div>
+    </motion>
   );
 }
