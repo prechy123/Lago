4619e9ffd061c7b8c3213931b518164e756b6696
prechy123 <bamideleprecious100@gmail.com>
2023-08-27 19:47:38 +0100

    Added transition to hero of about page

--- src/components/Sections/about/Hero.jsx
+++ src/components/Sections/about/Hero.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+const containerVariance = {
+  initial: {
+    x: "100vw",
+  },
+  animate: {
+    x: 0,
+    transition: {
+      duration: 1.5,
+    },
+  },
+};
 
 export default function Hero() {
   return (
-    <section id="hero">
+    <motion.section
+      id="hero"
+      variants={containerVariance}
+      initial="initial"
+      animate="animate"
+    >
       <div className="hero-title">
         <h1>The story of Lago</h1>
         <p>
@@ -11,13 +29,13 @@ export default function Hero() {
         </p>
       </div>
       <div className="grid-container">
-          <div className="grid-item-1"></div>
-          <div className="grid-item-2"></div>
-          <div className="grid-item-3"></div>
-          <div className="grid-item-4"></div>
-          <div className="grid-item-5"></div>
-          <div className="grid-item-6"></div>
+        <div className="grid-item-1"></div>
+        <div className="grid-item-2"></div>
+        <div className="grid-item-3"></div>
+        <div className="grid-item-4"></div>
+        <div className="grid-item-5"></div>
+        <div className="grid-item-6"></div>
       </div>
-    </section>
+    </motion.section>
   );
 }
