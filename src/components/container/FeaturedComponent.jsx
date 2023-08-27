f4aeb7f0e7f198acbf0be07863ba34d3deefda4f
prechy123 <bamideleprecious100@gmail.com>
2023-08-27 18:56:21 +0100

    animated fetured component

--- src/components/container/FeaturedComponent.jsx
+++ src/components/container/FeaturedComponent.jsx
@@ -1,12 +1,12 @@
-
+import { motion } from "framer-motion";
 export default function FeaturedComponent({ title, details }) {
   return (
-    <div className="featured-component">
+    <motion.div className="featured-component" whileHover={{scale: 0.9}} initial={{y: "100px"}} whileInView={{y: 0}}>
       <div className="image"></div>
       <div className="content">
         <h4>{title}</h4>
         <p>{details}</p>
       </div>
-    </div>
+    </motion.div>
   );
 }
